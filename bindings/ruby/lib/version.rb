627f20380fe937f3382165f994aa344ed464a42f
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-07-23 21:38:58 -0700

    v0.2.0-alpha.6

--- bindings/ruby/lib/version.rb
+++ bindings/ruby/lib/version.rb
@@ -1,4 +1,4 @@
 # automatically generated, do not edit by hand.
 module SqliteLines
-  VERSION = "0.2.0-alpha.5"
+  VERSION = "0.2.0-alpha.6"
 end
--- bindings/sqlite-utils/sqlite_utils_sqlite_lines/version.py
+++ bindings/sqlite-utils/sqlite_utils_sqlite_lines/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.1.0-alpha.61"
+__version__ = "0.2.0-alpha.6"
 __version_info__ = tuple(__version__.split("."))
