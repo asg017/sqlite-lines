e8bd1c357bb2d6080f3e9eeda692e801b29f801d
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-08-06 00:05:27 -0700

    deno fix v

---
+++ examples/deno/main.ts
@@ -0,0 +1,14 @@
+import { Database } from "https://deno.land/x/sqlite3/mod.ts";
+import * as sqlite_lines from "https://deno.land/x/sqlite_lines/mod.ts";
+import sqlite_lines_meta from "https://deno.land/x/sqlite_lines/deno.json" assert { type: "json" };
+
+console.log(`sqlite_lines_meta version:`, sqlite_lines_meta.version);
+``;
+const db = new Database(":memory:");
+
+db.enableLoadExtension = true;
+db.loadExtension(sqlite_lines.getLoadablePath());
+
+const [version] = db.prepare("select lines_version()").value<[string]>()!;
+
+console.log(version);
